9d75f5941cdf82ab7a8faba60423811861088526
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2016-11-18 15:11:29 +0100

    Replaced the request/response based portal module with the SPA based portal.js module

--- portal/app/js/controllers-org.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("MyApp", []);
-
-app.controller("PostsCtrl", function($scope, $http) {
-  $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-  $http.get('data/posts.json').
-    success(function(data, status, headers, config) {
-      $scope.posts = data;
-    });
-});
