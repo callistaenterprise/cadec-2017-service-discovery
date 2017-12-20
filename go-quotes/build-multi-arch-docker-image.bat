9e81c5121595dc811405e7d6293d93030ec86e0f
magnus <magnus.larsson.ml@gmail.com>
2017-12-20 14:39:04 +0100

    added a manifest file for a multi arch Docker image

--- go-quotes/build-multi-arch-docker-image.bat
+++ go-quotes/build-multi-arch-docker-image.bat
@@ -23,6 +23,28 @@ docker build -f Dockerfile-windows-amd64 -t magnuslarsson/quotes:24-go-windows-a
 docker push magnuslarsson/quotes:24-go-windows-amd64
 docker run -d -p 8080:8080 --name quotes magnuslarsson/quotes:24-go-windows-amd64
 
+
+> curl http://192.168.1.224:8080/api/quote -UseBasicParsing
+>>
+StatusCode        : 200
+StatusDescription : OK
+Content           : {"hardwareArchitecture":"amd64","operatingSystem":"windows","ipAddress":"4d131b511ab9/fe80::9846:5b
+                    e3:c0bb:2d91%Ethernet172.24.224.172","quote":"In Go, the code does exactly what it says on the page
+                    ."...
+RawContent        : HTTP/1.1 200 OK
+                    Content-Length: 217
+                    Content-Type: application/json
+                    Date: Wed, 20 Dec 2017 12:07:09 GMT
+
+                    {"hardwareArchitecture":"amd64","operatingSystem":"windows","ipAddress":"4d131b511ab9/fe80:...
+Forms             :
+Headers           : {[Content-Length, 217], [Content-Type, application/json], [Date, Wed, 20 Dec 2017 12:07:09 GMT]}
+Images            : {}
+InputFields       : {}
+Links             : {}
+ParsedHtml        :
+RawContentLength  : 217
+
 REM LINUX VERSION
 
 set GOOS=linux
@@ -32,3 +54,32 @@ REM export GOOS=darwin
 docker build -f Dockerfile-linux-amd64 -t magnuslarsson/quotes:24-go-linux-amd64 .
 docker push magnuslarsson/quotes:24-go-linux-amd64
 docker run -d -p 8080:8080 --name quotes magnuslarsson/quotes:24-go-linux-amd64
+
+
+
+> curl http://localhost:8080/api/quote -UseBasicParsing
+>>
+
+StatusCode        : 200
+StatusDescription : OK
+Content           : {"hardwareArchitecture":"amd64","operatingSystem":"linux","ipAddress":"0c4e0824f479/172.17.0.2","qu
+                    ote":"I like a lot of the design decisions they made in the [Go] language. Basically, I like all of
+                     t...
+RawContent        : HTTP/1.1 200 OK
+                    Content-Length: 222
+                    Content-Type: application/json
+                    Date: Wed, 20 Dec 2017 11:44:37 GMT
+
+                    {"hardwareArchitecture":"amd64","operatingSystem":"linux","ipAddress":"0c4e0824f479/172.17....
+Forms             :
+Headers           : {[Content-Length, 222], [Content-Type, application/json], [Date, Wed, 20 Dec 2017 11:44:37 GMT]}
+Images            : {}
+InputFields       : {}
+Links             : {}
+ParsedHtml        :
+RawContentLength  : 222
+
+
+REM MULTI-ARCH VERSION
+
+manifest-tool-windows-amd64.exe  --docker-cfg "\Users\magnus\.docker\config.json" push from-spec manifest-quotes-multiarch.yml
